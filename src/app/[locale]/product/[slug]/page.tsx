a67e578a34855bb0647e887356a31401874a9693
Ayoub <ayoub@nouamane.ma>
2026-07-12 12:20:25 +0100

    fix: set product page to force-dynamic to bypass cache issues for new coffrets

--- src/app/[locale]/product/[slug]/page.tsx
+++ src/app/[locale]/product/[slug]/page.tsx
@@ -6,7 +6,8 @@ import { Product } from '@/lib/products';
 
 import { Metadata } from 'next';
 
-export const revalidate = 3600;
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
 
 export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
   const resolvedParams = await params;
